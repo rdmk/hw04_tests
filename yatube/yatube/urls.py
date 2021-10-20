899da196e7ff63bfb5bb50c9e72003b4e540aa48
Erdem <munkueveg@gmail.com>
2021-10-21 03:21:53 +0800

    block comment should start with '# '

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('about/', include('about.urls', namespace='about')),
 ]
 
-#handler404 = 'core.views.page_not_found'
-#handler500 = 'core.views.server_error'
-#handler403 = 'core.views.permission_denied'
+# handler404 = 'core.views.page_not_found'
+# handler500 = 'core.views.server_error'
+# handler403 = 'core.views.permission_denied'
 
 if settings.DEBUG:
     urlpatterns += static(
